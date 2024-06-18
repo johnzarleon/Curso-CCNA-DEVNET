61afdc743b6d65ba74fcbd0a095b22208a62efbe
johan meza <jo.meza@duocuc.cl>
2024-06-18 21:05:00 +0000

    Agregar script de solicitud de datos en Python

---
+++ johan.py
@@ -0,0 +1,10 @@
+def solicitar_datos():
+    nombre = input("Ingrese su nombre: ")
+    apellido = input("Ingrese su apellido: ")
+    edad = input("Ingrese su edad: ")
+
+    print(f"\nDatos ingresados:\nNombre: {nombre}\nApellido: {apellido}\nEdad: {edad}")
+
+if __name__ == "__main__":
+    solicitar_datos()
+#johan meza
